93d22329165ac45557ddfda05cb5b4dfb3a27aeb
sandeep <vattyam.sandeep@gmail.com>
2024-12-09 10:39:12 +0530

    Implement the Add button

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
      to do items.
       """
@@ -7,7 +9,7 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write to-do item list to text file """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
